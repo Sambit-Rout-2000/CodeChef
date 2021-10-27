cc4fdd98cd35d8bd843d121bdf0309aa0ecccde1
Sambit Rout <93064901+Sambit-Rout-2000@users.noreply.github.com>
2021-10-27 19:04:17 +0530

    Create Small Factorials.java

---
+++ Practice/Beginner/Java/Small Factorials.java
@@ -0,0 +1,65 @@
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+import java.io.*;
+public class Main
+	{
+	static class FastReader
+		{
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader()
+			{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			}
+		String next()
+			{
+			while (st == null || !st.hasMoreElements())
+				{
+				try {
+				st = new StringTokenizer(br.readLine());
+				}catch (IOException e)
+					{
+					e.printStackTrace();
+					}
+				}
+			return st.nextToken();
+			} 
+		int nextInt()
+			{
+			return Integer.parseInt(next());
+			}
+		long nextLong()
+			{
+			return Long.parseLong(next());
+			}
+		double nextDouble()
+			{
+			return Double.parseDouble(next());
+			}
+		String nextLine()
+			{
+			String str = "";
+			try {
+				str = br.readLine();
+			}catch (IOException e)
+				{
+				e.printStackTrace();
+				}
+			return str;
+			}
+		}
+	public static void main(String args[])throws java.lang.Exception
+		{
+		FastReader sc = new FastReader();
+		int t = sc.nextInt();
+		while(t-->0)
+			{
+			int num = sc.nextInt();
+			BigInteger f = BigInteger.ONE;
+			for (int i = 2; i <= num; i++)
+				f = f.multiply(BigInteger.valueOf(i));
+			System.out.println(f);
+			}
+		}
+	}
