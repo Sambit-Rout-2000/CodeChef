61707fa9740425ffb5c792c0f2e7f78931fd79b2
Sambit Rout <93064901+Sambit-Rout-2000@users.noreply.github.com>
2021-11-24 07:34:15 +0530

    Create Primality Test.java

---
+++ Practice/Beginner/Java/Primality Test.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main
+	{
+	static class FastReader
+		{
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader()
+			{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			}
+		String next()
+			{
+			while (st == null || !st.hasMoreElements())
+				{
+				try {
+				st = new StringTokenizer(br.readLine());
+				}catch (IOException e)
+					{
+					e.printStackTrace();
+					}
+				}
+			return st.nextToken();
+			} 
+		int nextInt()
+			{
+			return Integer.parseInt(next());
+			}
+		long nextLong()
+			{
+			return Long.parseLong(next());
+			}
+				//written by Sambit Rout
+		double nextDouble()
+			{
+			return Double.parseDouble(next());
+			}
+		String nextLine()
+			{
+			String str = "";
+			try {
+				str = br.readLine();
+			}catch (IOException e)
+				{
+				e.printStackTrace();
+				}
+			return str;
+			}
+		}
+	public static void main (String[] args) throws java.lang.Exception
+		{
+		FastReader sc = new FastReader();
+		int t = sc.nextInt();
+		while(t-->0)
+			{
+			int A = sc.nextInt();
+			int count=0;
+			if(A==1)
+				{
+				System.out.println("no");
+				continue;
+				}
+			else
+				{
+				for (int i=2;i<A;i++)
+					{
+					if(A%i==0)
+						{
+						count++;
+						break;
+						}
+					}
+				}
+			if(count==0)
+				{
+				System.out.println("yes");
+				}
+			else
+				{
+				System.out.println("no");
+				}
+			}
+		}
+	}
+	//written by Sambit Rout
