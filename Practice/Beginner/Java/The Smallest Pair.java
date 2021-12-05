7ad725bbc8e220f2932f042403913497a37f3205
Sambit Rout <93064901+Sambit-Rout-2000@users.noreply.github.com>
2021-12-05 09:41:41 +0530

    Create The Smallest Pair.java

---
+++ Practice/Beginner/Java/The Smallest Pair.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main
+	{
+	static class FastReader
+		{
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader()
+			{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			}
+		String next()
+			{
+			while (st == null || !st.hasMoreElements())
+				{
+				try {
+				st = new StringTokenizer(br.readLine());
+				}catch (IOException e)
+					{
+					e.printStackTrace();
+					}
+				}
+			return st.nextToken();
+			} 
+		int nextInt()
+			{
+			return Integer.parseInt(next());
+			}
+		long nextLong()
+			{
+			return Long.parseLong(next());
+			}
+				//written by Sambit Rout
+		double nextDouble()
+			{
+			return Double.parseDouble(next());
+			}
+		String nextLine()
+			{
+			String str = "";
+			try {
+				str = br.readLine();
+			}catch (IOException e)
+				{
+				e.printStackTrace();
+				}
+			return str;
+			}
+		}
+	public static void main (String[] args) throws java.lang.Exception
+		{
+		FastReader sc = new FastReader();
+		int t = sc.nextInt();
+		while(t-->0)
+			{
+			int D = sc.nextInt();
+			int arr[] = new int[D];
+			for(int i=0;i<D;i++)
+				{
+				arr[i] = sc.nextInt();
+				}
+			Arrays.sort(arr);
+			System.out.println(arr[0]+arr[1]);
+			}
+		}
+	}
+	//written by Sambit Rout
