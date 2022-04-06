bce5528477d13bf1406885b8bc68dfa03f055ed1
Sambit Rout <93064901+Sambit-Rout-2000@users.noreply.github.com>
2022-04-06 07:20:14 +0530

    Create Main.cpp

---
+++ Practice/0 - 1000 : Beginner Level/Email Reminders/C++/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+	{
+	// your code goes here
+	int U, N;
+	std::cin >>U>>N;
+	int ans = U - N;
+	std::cout << ans << std::endl;
+	return 0;
+	}
